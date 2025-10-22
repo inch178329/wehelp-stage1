b2297653f5eb2ec908bdb311f0d8998cc788f77a
inch <carol98569@gmail.com>
2025-10-22 22:34:30 +0800

    feat(js): complete Task 3 solution in assign2.js

--- week2/assign2.js
+++ week2/assign2.js
@@ -1,1 +1,23 @@
-console.log("Hello, WeHelp Week 2!");
+
+
+
+
+
+function func3(index) {
+    let ary = [];
+    let newValue = 25;
+    let aryRules = [-2,-3,+1,+2];
+    
+    for (let i = 0; i < 50; i++) {
+        ary.push(newValue);
+        let rule = aryRules[i%aryRules.length];
+        newValue = newValue+rule;
+    }
+    console.log(ary[index]);
+    
+}
+console.log("=== Task 3 ===");
+func3(1);  // print 23 
+func3(5);  // print 21 
+func3(10);  // print 16 
+func3(30);  // print 6
